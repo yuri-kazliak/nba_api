bc2870dddfa2e9a6a076278aa1fc962b05b07790
Yuri Kazliak <ykazliak@remedypointsolutions.com>
2024-11-08 17:23:31 +0300

    more data

--- src/nba_stats.py
+++ src/nba_stats.py
@@ -6,12 +6,16 @@ SCOREBOARD_URL = 'https://nba-prod-us-east-1-mediaops-stats.s3.amazonaws.com/NBA
 SINGLE_GAME_URL = 'https://core-api.nba.com/cp/api/v1.8/gameDetails'
 
 statistic_minumum_criteria = {
-  'assists': 3,
-  'blocks': 2,
   'points': 9,
   'reboundsTotal': 5,
-  'steals': 2
+  'assists': 3,
+  'steals': 2,
+  'blocks': 2
 }
+
+players_to_watch = [
+  "V. Wembanyama"
+]
 
 async def get_todays_scoreboard():
   r = httpx.get(SCOREBOARD_URL)
@@ -38,23 +42,33 @@ def parse_statline(stat_line):
   }
 
 def format_team_statline(team):
-  team_title = team['teamCity'] + ' ' + team['teamName']
+  team_title = f'{team['teamCity']} {team['teamName']}'
   players = []
   for player in team['players']:
     if player['status'] == 'ACTIVE':
+      criterias = statistic_minumum_criteria
       player_to_append = {
         'name': player['nameI']
       }
-      if player['statistics']['assists'] > statistic_minumum_criteria['assists']:
-        player_to_append['assists'] = player['statistics']['assists']
-      if player['statistics']['blocks'] > statistic_minumum_criteria['blocks']:
-        player_to_append['blocks'] = player['statistics']['blocks']
-      if player['statistics']['points'] > statistic_minumum_criteria['points']:
+      if player_to_append['name'] in players_to_watch:
+        criterias = {
+          'points': 1,
+          'reboundsTotal': 0,
+          'assists': 0,
+          'steals': 0,
+          'blocks': 0
+        }
+
+      if player['statistics']['points'] > criterias['points']:
         player_to_append['points'] = player['statistics']['points']
-      if player['statistics']['reboundsTotal'] > statistic_minumum_criteria['reboundsTotal']:
+      if player['statistics']['reboundsTotal'] > criterias['reboundsTotal']:
         player_to_append['reboundsTotal'] = player['statistics']['reboundsTotal']
-      if player['statistics']['steals'] > statistic_minumum_criteria['steals']:
+      if player['statistics']['assists'] > criterias['assists']:
+        player_to_append['assists'] = player['statistics']['assists']
+      if player['statistics']['steals'] > criterias['steals']:
         player_to_append['steals'] = player['statistics']['steals']
+      if player['statistics']['blocks'] > criterias['blocks']:
+        player_to_append['blocks'] = player['statistics']['blocks']
 
       if len(player_to_append) > 1:
         if not 'points' in player_to_append:
@@ -73,12 +87,12 @@ async def get_stats():
 
   # print(formatted['scoreboard']['games'])
 
-  print(list(map(lambda game: game['gameId'],formatted['scoreboard']['games'])))
+  # print(list(map(lambda game: game['gameId'],formatted['scoreboard']['games'])))
 
   games_stats = await asyncio.gather(*list(map(lambda game: get_single_game_info(game['gameId']),formatted['scoreboard']['games'])))
   formatted_game_stats = list(map(lambda stat: parse_statline(stat), games_stats))
 
-  print(formatted_game_stats)
+  # print(formatted_game_stats)
 
   # await get_single_game_info('0022400154')
   return {
@@ -86,6 +100,6 @@ async def get_stats():
     'League': formatted['scoreboard']['leagueName'],
     # 'Games': formatted['scoreboard']['games'],
     # 'GamesIds': list(map(lambda game: game['gameId'],formatted['scoreboard']['games']))
-    # 'Games Stats': games_stats,
-    'Games Stats': formatted_game_stats
+    'Games Stats': formatted_game_stats,
+    # 'formatted': formatted
   }
