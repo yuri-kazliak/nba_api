277c4618a50245ccdaec277a4a777539df6423bd
Yuri Kazliak <ykazliak@remedypointsolutions.com>
2024-11-11 16:07:36 +0300

    add setnry

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # nba_api
 
 poetry run uvicorn src.main:app --reload
-
-
-docker run -d --name nba_api -p 8018:8000 yurasick/my_basket_api:0.0.1
--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+import sentry_sdk
 
 from .nba_stats import get_stats
+
+sentry_sdk.init(
+    dsn="https://901ada8349b62d0c4ebe34c79816c81c@o4508279705960448.ingest.de.sentry.io/4508279708516432",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
 
 app = FastAPI()
 
