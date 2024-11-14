c27e8a4ab0de70f9563eadbf6fd0d893d466ea97
Yuri Kazliak <ykazliak@remedypointsolutions.com>
2024-11-14 12:54:34 +0300

    add refrech route

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ async def root():
     if not "todays_boxscore" in app_state or not app_state["todays_boxscore"]:
         await update_app_state_boxscore()
     else:
-        if datetime.now() > app_state["todays_boxscore_datetime"] + timedelta(hours=2):
+        if datetime.now() > app_state["todays_boxscore_datetime"] + timedelta(hours=1):
             asyncio.gather(update_app_state_boxscore())
 
     return app_state["todays_boxscore"]
@@ -58,11 +58,20 @@ async def players_stats():
         await update_app_state_players_stats()
     else:
         if datetime.now() > app_state["all_players_stats_datetime"] + timedelta(
-            hours=12
+            hours=1
         ):
             asyncio.gather(update_app_state_players_stats())
 
     return app_state["all_players_stats"]
+
+
+@app.get("/refresh")
+async def players_stats():
+    try:
+        asyncio.gather(update_app_state_boxscore(), update_app_state_players_stats())
+        return "State has been refreshed!"
+    except Exception as err:
+        return err
 
 
 # @app.get("/players-stats/{player_name}")
